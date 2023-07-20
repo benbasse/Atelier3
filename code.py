455e98062b6aab279220ce6d3450de3b39e97b0b
Moussa Basse <moussabasse221@gmail.com>
2023-07-20 13:24:59 +0000

    Ajout du fichier code.py

--- code.py
+++ code.py
@@ -1,6 +1,8 @@
 
-nombre1 = float(input("Entrez le premier nombre : "))
-nombre2 = float(input("Entrez le deuxieme bombre : "))
-
-somme = nombre1 + nombre2
-print("La somme des deux nombres est :", somme )
+def additionner(nombre1, nombre2):
+    somme = nombre1 + nombre2
+    return somme
+nombre1 = 2
+nombre2 = 7
+resultat = additionner(nombre1, nombre2)
+print("La somme des deux nombres est : ", resultat )
